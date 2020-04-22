87a019b151561fd26adc6b32277663597ee289cd
yuanpei <michaelteng0706@gmail.com>
2020-04-22 23:29:24 +0100

    Just let user know which rectangle they selected,but cant perform dragged.

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -8,7 +8,9 @@ import processing.data.TableRow;
 public class Gantt extends PApplet
 {
 	public ArrayList<Task> tasks = new ArrayList<Task>();
-
+	
+	public int check = 0;
+	
 	
 	public void settings()
 	{
@@ -41,8 +43,9 @@ public class Gantt extends PApplet
 		float left_side = width*0.05f;
 		float single_area = (width-border-border-70)/29;
 		float c = 0 ;
+
 		colorMode(HSB);
-		//float alt = (float)72/10;
+		
 
 		//draw the line and the number of the Days
 		stroke(255);
@@ -61,14 +64,14 @@ public class Gantt extends PApplet
 		{
 			
 			float y = map(z,0,tasks.size(),border_height,height-200);
+			stroke(0);
 			fill(c,255,255);
-			rect(border+70+(tasks.get(z).getStart()-1)*single_area , y+15 , (tasks.get(z).getEnd()-tasks.get(z).getStart())*single_area ,30,5);
+			rect(border+70+(tasks.get(z).getStart()-1)*single_area , y+15 , (tasks.get(z).getEnd()-tasks.get(z).getStart())*single_area,30,5);
 			
 			fill(255);
 			text(tasks.get(z).getTask(), left_side ,y+(border_height/2));
 			c = c + 27;
-			
-			
+		
 		}
 
 
@@ -78,15 +81,64 @@ public class Gantt extends PApplet
 	
 	public void mousePressed()
 	{
-		println("Mouse pressed");	
+
+			if(mouseY > 75 && mouseY < 105)
+			{
+				check = 1;
+				println(tasks.get(0).getTask() + " Selected");
+
+			}
+			if(mouseY >112.77  && mouseY < 142.77)
+			{
+				check = 2;
+				println(tasks.get(1).getTask() + " Selected");
+			}
+			if(mouseY > 150.55 && mouseY < 180.55)
+			{
+				check = 3;
+				println(tasks.get(2).getTask() + " Selected");
+			}
+			if(mouseY > 188.33 && mouseY < 218.33)
+			{
+				check = 4;
+				println(tasks.get(3).getTask() + " Selected");
+			}
+			if(mouseY > 226.11 && mouseY < 256.11)
+			{
+				check = 5;
+				println(tasks.get(4).getTask() + " Selected");
+			}
+			if(mouseY > 263.88 && mouseY < 293.88)
+			{
+				check = 6;
+				println(tasks.get(5).getTask() + "Selected");
+			}
+			if(mouseY > 301.66 && mouseY < 331.66)
+			{
+				check = 7;
+				println(tasks.get(6).getTask() + "Selected");
+			}
+			if(mouseY > 339.44 && mouseY < 369.44)
+			{
+				check = 8;
+				println(tasks.get(7).getTask() + "Selected");
+			}
+			if(mouseY > 377.22 && mouseY < 407.22)
+			{
+				check = 9;
+				println(tasks.get(8).getTask() + "Selected");
+			}
+
+			
 	}
 
 	public void mouseDragged()
 	{
-		println("Mouse dragged");
+		
+		println("mousedragged");
+		
 	}
 
-	
 	
 	public void setup() 
 	{
